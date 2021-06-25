93bd785f7c168ce7cd80a55b9270c09d61fce873
Mike Bourgeous <mike@mikebourgeous.com>
2021-06-25 10:40:48 -0600

    Set up RSpec and disable rubygem-specific message to use git.

--- lib/mb/surround.rb
+++ lib/mb/surround.rb
@@ -1,7 +1,10 @@
+require 'mb-sound'
+require 'mb-geometry'
+
 require_relative 'surround/version'
 
 module MB
   module Surround
-    raise 'Pre-release gem.  Please use Git directly at https://github.com/mike-bourgeous/mb-surround.git'
+    # TODO
   end
 end
---
+++ spec/lib/mb/surround_spec.rb
@@ -0,0 +1,3 @@
+RSpec.describe(MB::Surround) do
+  pending
+end
